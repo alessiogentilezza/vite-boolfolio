533fad51c0c5cb50a5780dccd1ded42076aadf6d
alessiogentilezza <alessio.gentilezza@gmail.com>
2023-05-30 17:04:26 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script>
+  import AppMain from './components/AppMain.vue';
+
+  export default {
+    components: {
+      AppMain
+    }
+  }
+
+</script>
+
+<template>
+  <AppMain></AppMain>
+</template>
+
+<style scoped>
+
+</style>
